a769ec69ff149697fa3b3589ee57fca21eadb46c
alaa <alaaalh437@gamil.com>
2022-02-08 04:45:45 +0200

    make improve in style

--- book-store/component/Layout/Header.js
+++ book-store/component/Layout/Header.js
@@ -6,7 +6,8 @@ import classes from "./Header.module.css";
 import HeaderButton from "./HeaderButton";
 
 const Header = (props) => {
-  console.log(props.children);
+  
+
   return (
     <Fragment>
       <header className={classes.header}>
@@ -14,13 +15,13 @@ const Header = (props) => {
         <HeaderButton>{props.children}</HeaderButton>
       </header>
       <div className={classes["main-image"]}>
+      <div>
         <Image
           src={mainImage}
           alt="a girl write in book"
-          width="110%"
-          height="35%"
-          layout="responsive"
+          layout="fill"
         />
+        </div>
       </div>
     </Fragment>
   );
